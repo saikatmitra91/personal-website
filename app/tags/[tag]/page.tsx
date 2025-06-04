712191047b1b8e2646cf626eca4cb2a3e09e61c6
Saikat Mitra <saikatmitra91@gmail.com>
2025-06-04 12:16:10 +0530

    fix tag page title generation (#4)

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -33,7 +33,8 @@ export const generateStaticParams = async () => {
 export default function TagPage({ params }: { params: { tag: string } }) {
   const tag = decodeURI(params.tag)
   // Capitalize first letter and convert space to dash
-  const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
+  const dashed = tag.split(' ').join('-')
+  const title = dashed.charAt(0).toUpperCase() + dashed.slice(1)
   const filteredPosts = allCoreContent(
     sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
   )
